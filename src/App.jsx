c9e7eae7f9c5be0db7f4e0d9fdd091702861053c
François Martineau <87383252+franccy29@users.noreply.github.com>
2021-12-07 15:38:50 -0500

    started home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,39 @@
 import React, { useEffect, useRef } from 'react';
 import { gsap } from "gsap";
+import { StyledApp } from "./styles/App.styles.jsx";
+import react from "./image/react.png";
+import GSAP from "./image/GSAP.png";
+import JS from "./image/JS.png";
+import nodejs from "./image/nodejs.png";
 
 function App() {
 
-  const boxRef = useRef();
+  const name = useRef();
+  const profession = useRef();
+  const intro = useRef();
+  const images = useRef();
 
   useEffect(() => {
-    gsap.to(boxRef.current, { x: 100 });
+    gsap.to(name.current, { duration: 2.5, x: 100 });
+    gsap.to(profession.current, { duration: 2.5, x: 100 });
+    gsap.to(intro.current, { duration: 2.5, x: 100 });
+    gsap.to(images.current, { duration: 2.5, x: 100 });
   });
 
   return (
-    <div className="App">
-      <p ref={boxRef} >
-        test
-      </p>
-    </div>
+    <StyledApp>
+      <span className={"nom"} ref={name} >François Martineau</span>
+      <span className={"profession"} ref={profession} >Developpeur web</span>
+      <div className={"intro"} ref={intro} >
+        Bienvenue! Je suis un developpeur web full-stack dans la région de Montréal, QC.
+      </div>
+      <div className={"images"} ref={images} >
+        <img src={react} alt="React" />
+        <img src={JS} alt="Java Script" />
+        <img src={GSAP} alt="GSAP" />
+        <img src={nodejs} alt="nodeJS" />
+      </div>
+    </StyledApp>
   );
 }
 
---
+++ src/styles/App.styles.jsx
@@ -0,0 +1,26 @@
+import styled from 'styled-components'
+
+export const StyledApp = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+
+  & img {
+    height: 100px;
+    width: 100px;
+    margin: 0 0.7em;
+  }
+
+  & .nom {
+    font-size: 1.8em;
+  }
+
+  & .profession {
+    color: gray;
+    padding-left: 2em;
+  }
+  & .intro {
+    font-size: 3em;
+    padding: 1em 0;
+  }
+`;
